d42c1803c2a80f043f8fbe1b2a2367293902f161
Xavier Lu <xll2@cornell.edu>
2021-03-07 23:26:20 -0500

    added contract abi and input data decoder

--- web3-http.js
+++ web3-http.js
@@ -1,10 +1,15 @@
 import Web3 from "web3";
+import InputDataDecoder from "ethereum-input-data-decoder";
 import AWSHttpProvider from "./aws-http-provider.js";
 const endpoint = process.env.AMB_HTTP_ENDPOINT;
 const web3 = new Web3(new AWSHttpProvider(endpoint));
 
+const decoder = new InputDataDecoder(`contract_abi.json`);
+
 web3.eth
   .getTransaction(
-    "0x6ea40bdf299092b527c107a479c3062df43af83d81ba372e0740372d3251deab"
+    "0xd82a86f8324fba7e0d374b461d6faf0c39a0d53fde06505d6c2cb8447609c617"
   )
-  .then(console.log);
+  .then((result) => {
+    console.log(decoder.decodeData(result.input));
+  });
